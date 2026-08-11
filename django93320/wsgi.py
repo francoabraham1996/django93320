55f3ea17cea9cf5c562ab37bcc9ec6fbfeea8382
Franco Abraham <franco.abraham1996@gmail.com>
2026-08-10 21:37:48 -0300

    Creo proyecto Django

---
+++ django93320/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django93320 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django93320.settings')
+
+application = get_wsgi_application()
